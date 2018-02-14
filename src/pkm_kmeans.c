d0ad2df16b00265c9df58ea4d8e98ac575086c8e
Matej Klemen <matej.klemen1337@gmail.com>
2018-02-14 18:22:29 +0100

    Add custom structures, required for KMeans

--- src/pkm_kmeans.c
+++ src/pkm_kmeans.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef float pkm_datatype;
+typedef struct pkm_data_point
+{
+	// which centroid this point belongs to
+	int cluster_id;
+	pkm_datatype *data;
+} pkm_data_point;
+
+typedef struct pkm_centroid
+{
+	int cluster_id;
+	pkm_data_point *members;
+	pkm_datatype *center;
+} pkm_centroid;
 
 int main(int argc, char *argv[])
 {
